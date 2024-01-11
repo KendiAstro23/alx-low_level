b45a19b2076ff32d04fac5f4ee64b107ae77906f
KendiAstro23 <litalakendy975@gmail.com>
2024-01-11 17:56:57 -0600

    sum of data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - performs sum of data
+ * @head: head first
+ * Return: sum 
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
